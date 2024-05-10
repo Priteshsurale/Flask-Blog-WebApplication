6ff2a8201ad6f8f4fa8e27743d09470c11eed47a
SuralePritesh <pritesh.surale@netprotector.net>
2024-05-10 10:41:34 +0530

    Pagination added

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,27 +1,27 @@
-import os
-import secrets
-from PIL import Image
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
+from flaskblog.utils import save_picture
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask import render_template, url_for, flash, redirect, request, abort
 from flask_login import login_user, current_user, logout_user, login_required 
 
 
-# home route 
+# HOME ROUTE 
 @app.route("/")
 @app.route("/home")
 def home():
-  posts = Post.query.all()
+  page = request.args.get('page',1,type=int)
+  posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=5)
   return render_template('home.html',posts=posts, title='Home')
-  
-# about page route
+
+
+# ABOUT PAGE ROUTE
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
 
 
-# register page route
+# REGISTER PAGE ROUTE
 @app.route("/register", methods=['GET','POST'])
 def register():
   if current_user.is_authenticated:
@@ -33,14 +33,13 @@ def register():
     user = User(username=form.username.data,email=form.email.data, password=hash_pass)
     db.session.add(user)
     db.session.commit()
-    
     flash(f'Account created for {form.username.data}!','success')
     return redirect(url_for('login'))
     
   return render_template('register.html', title='Register', form=form)
 
 
-# login page route
+# LOGIN PAGE ROUTE
 @app.route("/login", methods=['GET','POST'])
 def login():
   if current_user.is_authenticated:
@@ -51,41 +50,29 @@ def login():
     user = User.query.filter_by(email=form.email.data).first()
     if user and bcrypt.check_password_hash(user.password, form.password.data):
       login_user(user, remember=form.remember.data)
+      
       # if any query parameter
       next_page = request.args.get('next')
-       
       return redirect(next_page) if next_page else redirect(url_for('home'))
     else:
       flash('Login Unsuccessful. Please check email and password','danger')
+  
   return render_template('login.html', title='Login', form=form)
 
 
-def save_picture(form_picture):
-  random_hex = secrets.token_hex(8)
-  _,f_ext = os.path.splitext(form_picture.filename)
-  picture_fn = random_hex + f_ext
-  picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
-  
-  output_size = (125,125)
-  i = Image.open(form_picture)
-  i.thumbnail(output_size)
-  i.save(picture_path)
-  return picture_fn
-
-# logout route
+# LOGOUT ROUTE
 @app.route('/logout')
 def logout():
   logout_user()
   return redirect(url_for('home'))
 
-# account route
+
+# ACCOUNT ROUTE
 @app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
   form = UpdateAccountForm()
-  
   if form.validate_on_submit():
-    
     if form.picture.data:
       picture = save_picture(form.picture.data)
       current_user.image_file = picture   
@@ -104,7 +91,7 @@ def account():
   return render_template('account.html', title='Account',image_file=image_file, form=form)
 
 
-# add posts 
+# ADD POSTS 
 @app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
@@ -115,17 +102,18 @@ def new_post():
     db.session.commit()
     flash('Your post has been created!', 'success')
     return redirect(url_for('home'))
+ 
   return render_template('create_post.html', title='New Post', form=form, legend='New Post') 
 
 
-# specified Id post
+# SPECIFIED ID POST
 @app.route('/post/<int:post_id>')
 def post(post_id):
   post = Post.query.get_or_404(post_id)
   return render_template('post.html',title=post.content, post=post)
 
 
-# update post
+# UPDATE POST
 @app.route('/post/<int:post_id>/update',methods=['GET','POST'])
 @login_required
 def update_post(post_id):
@@ -146,7 +134,7 @@ def update_post(post_id):
     
   return render_template('create_post.html', title='Update Post', form=form, legend='Update Post') 
 
-# update post
+# DELETE POST
 @app.route('/post/<int:post_id>/delete',methods=['GET','POST'])
 @login_required
 def delete_post(post_id):
@@ -160,3 +148,15 @@ def delete_post(post_id):
   flash('Your post has been deleted','success')
   return redirect(url_for('home'))
   
+
+# USER SPECIFIC POSTS
+@app.route("/user/<string:username>")
+def user_posts(username):
+  page = request.args.get('page',1,type=int)
+  user = User.query.filter_by(username=username).first_or_404()
+  
+  posts = Post.query.filter_by(author=user)\
+    .order_by(Post.date_posted.desc())\
+      .paginate(page=page,per_page=5)
+      
+  return render_template('user_post.html',posts=posts, user=user, title='User Post')
